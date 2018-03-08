74d71fad19f00fb9f502773c93e2aa691fea8441
radoycheva <radoycheva.e@gmail.com>
2018-03-08 16:59:16 +0200

    Delete article.js

--- owl-carousel/article.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-            $("#owl").owlCarousel({
-                navigation: false,
-                slideSpeed: 100,
-                paginationSpeed: 800,
-                singleItem: true,
-                autoPlay: false
-            });
-        });
