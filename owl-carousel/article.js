c7d9394bfae478cc6fe26b3efd132e6f37ebd643
radoycheva <radoycheva.e@gmail.com>
2018-03-08 16:56:58 +0200

    Add files via upload

---
+++ owl-carousel/article.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+            $("#owl").owlCarousel({
+                navigation: false,
+                slideSpeed: 100,
+                paginationSpeed: 800,
+                singleItem: true,
+                autoPlay: false
+            });
+        });
